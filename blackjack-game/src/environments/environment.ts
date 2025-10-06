615c34b34b0bb64a829a70cd7600763d70115b68
Aiken <chikitlee2001@gmail.com>
2025-10-07 04:43:04 +1100

    change type in string

--- blackjack-game/src/environments/environment.ts
+++ blackjack-game/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: process.env['NG_APP_SUPABASE_URL'],
-  supabaseKey: process.env['NG_APP_SUPABASE_KEY'],
-  geminiApiKey: process.env['NG_APP_GEMINI_API_KEY']
+  supabaseUrl: (process.env['NG_APP_SUPABASE_URL'] as string),
+  supabaseKey: (process.env['NG_APP_SUPABASE_KEY'] as string),
+  geminiApiKey: (process.env['NG_APP_GEMINI_API_KEY'] as string)
 };
