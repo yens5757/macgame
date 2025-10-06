376450d21d4c759260667902069230c197f421af
Aiken <chikitlee2001@gmail.com>
2025-10-07 01:07:17 +1100

    remove env for vercel

---
+++ blackjack-game/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: process.env['NG_APP_SUPABASE_URL'] || '',
+  supabaseKey: process.env['NG_APP_SUPABASE_KEY'] || '',
+  geminiApiKey: process.env['NG_APP_GEMINI_API_KEY'] || ''
+};
